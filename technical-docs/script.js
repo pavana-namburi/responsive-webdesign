3f9c37e2b1fa3b0af7ebaeb96e5944c559edc56e
pavana-namburi <pavananamburi1@gmail.com>
2025-09-11 21:47:34 +0530

    js

--- technical-docs/script.js
+++ technical-docs/script.js
@@ -1,13 +1,10 @@
-const toggle = document.getElementById("theme-switch");
 const body = document.body;
-const themeText = document.getElementById("theme-text");
+const sidebar = document.getElementById("sidebar");
 
-toggle.addEventListener("change", () => {
-    if (toggle.checked) {
-        body.classList.add("dark-mode");
-        themeText.textContent = "Light Mode";
-    } else {
-        body.classList.remove("dark-mode");
-        themeText.textContent = "Dark Mode";
-    }
-});
+function toggleTheme() {
+  body.classList.toggle("dark");
+}
+
+function toggleSidebar() {
+  sidebar.classList.toggle("active");
+}
